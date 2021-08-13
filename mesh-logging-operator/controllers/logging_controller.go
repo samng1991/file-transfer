29a6d450699bdfaa930f7758a2c0fa7724019261
Ng, Sam Y H <sam.yh.ng@hkjc.org.hk>
2021-08-13 18:02:10 +0800

    Test

--- mesh-logging-operator/controllers/logging_controller.go
+++ mesh-logging-operator/controllers/logging_controller.go
@@ -17,8 +17,8 @@ limitations under the License.
 package controllers
 
 import (
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
+	//appsv1 "k8s.io/api/apps/v1"
+	//corev1 "k8s.io/api/core/v1"
 
 	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
